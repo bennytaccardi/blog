2dc1ef89be10bb0557eec46991247236a5466227
axeowl <benny.taccardi@gmail.com>
2023-05-27 12:57:10 +0200

    Social buttons

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,8 @@ import { useState } from "react";
 import Home from "./home/home";
 
 export default function Root() {
-  const [count, setCount] = useState(0);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-tr from-black via-stone-900 to-black">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-tr bg-gradient-to-b from-zinc-900 via-gray-800 to-zinc-900">
         <Home />
     </main>
   );
