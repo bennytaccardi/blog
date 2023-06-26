c75d6f1bdf8336e068b92ede43088b5b48f4c323
axeowl <benny.taccardi@gmail.com>
2023-06-26 23:03:55 +0200

    Removing google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,6 @@ import { Analytics } from '@vercel/analytics/react';
 export default function Root() {
   return (
     <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3SJ8ZDVDK"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){
-          window.dataLayer.push(arguments)
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-C3SJ8ZDVDK');
-      </script>
       <main className="overflow-hidden scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-300 flex min-h-screen flex-col items-center justify-between pt-7 pb-10 bg-gradient-to-tr bg-gradient-to-b from-zinc-900 via-gray-800 to-zinc-900 overflow-auto scrollbar-hide">
         <Home />
       </main>
