ac440d537f05441f8818febd058dce5d602bdfbe
axeowl <benny.taccardi@gmail.com>
2023-06-26 23:02:47 +0200

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,22 @@
 // import Image from 'next/image'
 
 'use client';
-import Script from "next/script";
 import Home from "./home/home";
 import { Analytics } from '@vercel/analytics/react';
 
 export default function Root() {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.PUBLIC_GOOGLE_ANALYTICS}`}
-      />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3SJ8ZDVDK"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){
+          window.dataLayer.push(arguments)
+        }
+        gtag('js', new Date());
 
-      <Script strategy="lazyOnload">
-        {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-      </Script>
+        gtag('config', 'G-C3SJ8ZDVDK');
+      </script>
       <main className="overflow-hidden scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-300 flex min-h-screen flex-col items-center justify-between pt-7 pb-10 bg-gradient-to-tr bg-gradient-to-b from-zinc-900 via-gray-800 to-zinc-900 overflow-auto scrollbar-hide">
         <Home />
       </main>
